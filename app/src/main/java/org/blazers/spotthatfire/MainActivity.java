3895731eff4f7c80f925a054c8cea1614cc6fd28
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2018-10-20 19:40:13 +0000

    Linked report a fire button to ReportFireActivity

--- app/src/main/java/org/blazers/spotthatfire/MainActivity.java
+++ app/src/main/java/org/blazers/spotthatfire/MainActivity.java
@@ -1,13 +1,39 @@
 package org.blazers.spotthatfire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnReportFire, btnSafetyTips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReportFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reportIntent = new Intent(MainActivity.this,ReportFireActivity.class);
+                MainActivity.this.startActivity(reportIntent);
+            }
+        });
+
+        btnSafetyTips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to safety tips activity
+            }
+        });
+
+
+    }
+
+    private void init() {
+        btnReportFire = (Button) findViewById(R.id.btn_report_fire);
+        btnSafetyTips = (Button) findViewById(R.id.btn_safety_tips);
     }
 }
---
+++ app/src/main/java/org/blazers/spotthatfire/ReportFireActivity.java
@@ -0,0 +1,13 @@
+package org.blazers.spotthatfire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportFireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_fire);
+    }
+}
